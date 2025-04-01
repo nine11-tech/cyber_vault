1d6994b77727d0f0679a67e6392c837858f4ce14
Anass <indiahoir@gmail.com>
2025-04-01 17:56:49 +0000

    added missing routes

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,20 @@ Route::post('/login', [AuthController::class, 'login'])->name('auth.login');
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 Route::get('/register', [AuthController::class, 'showForm'])->name('register');
 Route::post('/register', [AuthController::class, 'register'])->name('auth.register');
+
+// ------------------ Public Cart Routes ------------------
+
+Route::get('/cart', [CartController::class, 'index'])->name('cart.index');
+Route::post('/cart/add/{id}', [CartController::class, 'add'])->name('cart.add');
+Route::put('/cart/update/{id}', [CartController::class, 'update'])->name('cart.update');
+Route::delete('/cart/remove/{id}', [CartController::class, 'remove'])->name('cart.remove');
+
+// ------------------ Paypal Checkout Routes ------------------
+
+use App\Http\Controllers\PaypalController;
+
+Route::get('/checkout', [PaypalController::class, 'getEmail'])->name('paypal.email');
+Route::post('/checkout', [PaypalController::class, 'captureEmail'])->name('paypal.capture.email');
+Route::get('/checkout/pay', [PaypalController::class, 'checkout'])->name('paypal.checkout');
+Route::get('/checkout/success', [PaypalController::class, 'success'])->name('paypal.success');
+Route::get('/checkout/cancel', [PaypalController::class, 'cancel'])->name('paypal.cancel');
